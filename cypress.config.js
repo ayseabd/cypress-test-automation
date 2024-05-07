2fea0d174cf09ce8e00a6503d6264aa129fa05b6
Ayşe Abdullahoğlu <ayse.abdullahoglu@yandex.com>
2024-05-07 20:52:01 +0300

    .gitignore added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "h31u7g",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
